c3f7c0ab2923f7fef2e84d0736ba94d6368e2e6c
Lucas Victor <lucassvtf@gmail.com>
2024-07-16 13:05:06 -0300

    feat: adds vehicles routes to the app.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
 import userRoutes from './routers/UserRoutes';
+import vehicleRoutes from './routers/VehicleRoutes';
 import errorMiddleware from './middlewares/errorMiddleware';
 
 const app = express();
 
 app.use(express.json());
 app.use(userRoutes);
+app.use(vehicleRoutes);
 
 app.use(errorMiddleware);
 
