40a8e1743923db632a654a8bd1f939294e97c59c
Lucas Victor <lucassvtf@gmail.com>
2024-07-16 11:49:45 -0300

    feat: creates a abstract model.

---
+++ src/models/Abstract.ts
@@ -0,0 +1,32 @@
+import { model, Model, models, Schema } from 'mongoose';
+
+export default class ModelExample<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, schema);
+  }
+
+  public async create(item: T): Promise<T> {
+    return this.model.create(item);
+  }
+
+  public async findOne(query: object): Promise<T | null> {
+    return this.model.findOne(query);
+  }
+  public async findById(_id: string): Promise<T | null> {
+    return this.model.findById(_id);
+  }
+
+  public async update(_id: string, update: object): Promise<void> {
+    await this.model.updateOne({ _id }, update);
+  }
+
+  public async delete(_id: string): Promise<void> {
+    await this.model.deleteOne({ _id });
+  }
+}
