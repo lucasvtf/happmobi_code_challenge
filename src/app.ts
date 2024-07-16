687ea498f43dc8afcc963575350cfb8a9960a00d
Lucas Victor <lucassvtf@gmail.com>
2024-07-16 11:49:10 -0300

    feat: adds the routes to the app.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
+import userRoutes from './routers/UserRoutes';
+import errorMiddleware from './middlewares/errorMiddleware';
 
 const app = express();
 
 app.use(express.json());
+app.use(userRoutes);
+
+app.use(errorMiddleware);
 
 export default app;
