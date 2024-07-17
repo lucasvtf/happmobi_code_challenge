39b1e1f1e66eac14996921019939a17d4cf449f8
Lucas Victor <lucassvtf@gmail.com>
2024-07-17 15:49:41 -0300

    feat: adds reservationRoutes on app.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import userRoutes from './routers/UserRoutes';
 import vehicleRoutes from './routers/VehicleRoutes';
+import reservationRoutes from './routers/ReservationRoutes';
 import errorMiddleware from './middlewares/errorMiddleware';
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(userRoutes);
 app.use(vehicleRoutes);
+app.use(reservationRoutes);
 
 app.use(errorMiddleware);
 
