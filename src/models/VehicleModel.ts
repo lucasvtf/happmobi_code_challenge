a6e2df8c6be9dbb8e39b12bdc0dae42f0289e778
Lucas Victor <lucassvtf@gmail.com>
2024-07-17 15:50:44 -0300

    feat: creates a model for ReservationModel.

--- src/models/VehicleModel.ts
+++ src/models/VehicleModel.ts
@@ -8,9 +8,13 @@ export default class VehicleModel extends ModelExample<IVehicle> {
       vehicleModel: { type: String, required: true },
       vehicleLicensePlate: { type: String, required: true, unique: true },
       reserved: { type: Boolean, required: true, default: false },
-      reservedBy: { type: String, default: null },
+      reservedBy: { type: Schema.Types.ObjectId, ref: 'User', default: null },
     });
     const modelName = 'vehicles';
     super(schema, modelName);
+  }
+
+  async findReservedBy(userId: string): Promise<IVehicle[]> {
+    return await this.model.find({ reservedBy: userId });
   }
 }
