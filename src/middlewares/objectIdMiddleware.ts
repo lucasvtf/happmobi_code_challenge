3ef5a73513b5f975586197fd8fee5028fe704ff3
Lucas Victor <lucassvtf@gmail.com>
2024-07-17 15:51:30 -0300

    feat: creates a middleware to check id the is correct for mongodb.

---
+++ src/middlewares/objectIdMiddleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { Types } from 'mongoose';
+
+export const objectIdMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId, vehicleId } = req.params;
+
+  try {
+    if (userId && !Types.ObjectId.isValid(userId)) {
+      return res.status(400).send({ error: 'Invalid id.' });
+    }
+
+    if (vehicleId && !Types.ObjectId.isValid(vehicleId)) {
+      return res.status(400).send({ error: 'Invalid id.' });
+    }
+
+    next(); // Avançar para o próximo middleware ou rota
+  } catch (err) {
+    res.status(400).send({ error: 'Invalid id.' });
+  }
+};
