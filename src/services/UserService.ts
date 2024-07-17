374edc64c9a25eda2898dd5025524530c8870005
Lucas Victor <lucassvtf@gmail.com>
2024-07-17 16:54:49 -0300

    feat: implements a method to get the reservation history.

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,23 +3,26 @@ import IUser from '../interfaces/IUser';
 import UserModel from '../models/UserModel';
 import ApiErrors from '../utils/apiErros';
 import { createToken } from '../utils/jwt';
+import ReservationHistoryModel from '../models/ReservationHistoryModel';
 
 export default class UserService {
-  private model: UserModel;
+  private userModel: UserModel;
+  private reservationHistoryModel: ReservationHistoryModel;
 
   constructor() {
-    this.model = new UserModel();
+    this.userModel = new UserModel();
+    this.reservationHistoryModel = new ReservationHistoryModel();
   }
 
   public async create(user: IUser) {
     const hashedPassword = await bcrypt.hash(user.password, 10);
     user.password = hashedPassword;
-    const newUser = this.model.create(user);
+    const newUser = this.userModel.create(user);
     return newUser;
   }
 
   public async getById(userId: string) {
-    const user = this.model.findById(userId);
+    const user = this.userModel.findById(userId);
     if (!user) throw new ApiErrors('Invalid mongo id', 422);
     return user;
   }
@@ -27,15 +30,15 @@ export default class UserService {
   public async update(userId: string, userUpdate: IUser) {
     const user = await this.getById(userId);
     if (!user) throw new ApiErrors('User not found.', 404);
-    return this.model.update(userId, userUpdate);
+    return this.userModel.update(userId, userUpdate);
   }
 
   public async delete(userId: string) {
-    return this.model.delete(userId);
+    return this.userModel.delete(userId);
   }
 
   public async login(user: IUser) {
-    const userLogin = await this.model.findByEmail(user.email);
+    const userLogin = await this.userModel.findByEmail(user.email);
     if (!userLogin) throw new ApiErrors('Wrong email or password.', 403);
     const validPassword = await bcrypt.compare(
       user.password,
@@ -46,5 +49,17 @@ export default class UserService {
 
     const token = createToken({ email: userLogin.email });
     return token;
+  }
+
+  public async getReservationHistory(userId: string) {
+    const reservationHistory = await this.reservationHistoryModel.find({
+      userId,
+    });
+
+    if (reservationHistory.length === 0) {
+      throw new ApiErrors('No reservation history found for this user.', 404);
+    }
+
+    return reservationHistory;
   }
 }
