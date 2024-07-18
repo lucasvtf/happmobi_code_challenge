804e0d4ac88f84638cba7ae56f167002183689a7
Lucas Victor <lucassvtf@gmail.com>
2024-07-18 12:20:04 -0300

    refactor: changes the name of the methods.

--- src/routers/VehicleRoutes.ts
+++ src/routers/VehicleRoutes.ts
@@ -6,21 +6,21 @@ import { objectIdMiddleware } from '../middlewares/objectIdMiddleware';
 const vehicleRoutes = Router();
 
 vehicleRoutes.post('/vehicle', authMiddleware, (req, res, next) =>
-  new VehicleController(req, res, next).createVehicle()
+  new VehicleController(req, res, next).create()
 );
 
 vehicleRoutes.put(
   '/vehicle/:vehicleId',
   authMiddleware,
   objectIdMiddleware,
-  (req, res, next) => new VehicleController(req, res, next).updateVehicle()
+  (req, res, next) => new VehicleController(req, res, next).update()
 );
 
 vehicleRoutes.delete(
   '/vehicle/:vehicleId',
   authMiddleware,
   objectIdMiddleware,
-  (req, res, next) => new VehicleController(req, res, next).deleteVehicle()
+  (req, res, next) => new VehicleController(req, res, next).delete()
 );
 
 vehicleRoutes.get('/vehicle', authMiddleware, (req, res, next) =>
