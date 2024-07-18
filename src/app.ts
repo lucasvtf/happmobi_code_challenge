04d5cda107f1b4e46e5c37f9a09299b808dce15e
Lucas Victor <lucassvtf@gmail.com>
2024-07-18 12:16:29 -0300

    chore: add lib helmet, morgan, cors and jest.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
 import userRoutes from './routers/UserRoutes';
 import vehicleRoutes from './routers/VehicleRoutes';
 import reservationRoutes from './routers/ReservationRoutes';
@@ -8,6 +11,17 @@ import errorMiddleware from './middlewares/errorMiddleware';
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan('tiny'));
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
 app.use(userRoutes);
 app.use(vehicleRoutes);
 app.use(reservationRoutes);
