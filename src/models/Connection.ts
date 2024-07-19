e242b07554b188ada4080df3ced1cc2501815d6c
Lucas Victor <lucassvtf@gmail.com>
2024-07-19 14:34:27 -0300

    chore: add dockerfile and docker-compose.

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -1,10 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/happmobi';
-
-const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL
-) => mongoose.connect(mongoDatabaseURI);
+const connectToDatabase = (mongoDatabaseURI = process.env.MONGO_URI) =>
+  mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
